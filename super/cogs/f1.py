32ec0838c7a4ecb9d67a1fcfb260446330fd0f9a
Simone Esposito <hi@simone.sh>
2020-07-16 01:45:34 +0200

    cogs/f1: Add pagination

--- super/cogs/f1.py
+++ super/cogs/f1.py
@@ -5,6 +5,7 @@ from ago import human
 from arrow import Arrow
 from discord.ext import commands
 from super.settings import SUPER_TIMEZONE, SUPER_F1_CALENDAR
+from contextlib import suppress
 
 
 class F1(commands.Cog):
@@ -23,12 +24,13 @@ class F1(commands.Cog):
         except:
             return self._calendar
 
-    async def get_events(self, num, ongoing=True):
-        lines, lines_on = [], []
+    async def get_events(self, num, page=0, more=False):
+        lines = []
         calendar = await self.calendar()
-        for event in sorted(calendar.events, key=lambda x: x.begin):
-            if event.end > Arrow.now() > event.begin and ongoing:
-                lines_on.append(f"**{event.name}** ongoing")
+        start = min(page * num, len(calendar.events) - num)
+        for event in sorted(calendar.events, key=lambda x: x.begin)[start:]:
+            if event.end > Arrow.now() > event.begin:
+                lines.append(f"**{event.name}** ongoing")
             elif event.begin > Arrow.now():
                 local_time = event.begin.to(SUPER_TIMEZONE)
                 lines.append(
@@ -40,7 +42,9 @@ class F1(commands.Cog):
                 )
             if len(lines) >= num:
                 break
-        return lines_on + lines
+        if more and len(calendar.events) - start - num:
+            lines.append(f"...and {len(calendar.events) - start - num} more")
+        return lines
 
     @commands.command(no_pm=True, pass_context=True)
     async def f1ns(self, ctx):
@@ -50,9 +54,15 @@ class F1(commands.Cog):
 
     @commands.command(no_pm=True, pass_context=True)
     async def f1ls(self, ctx):
-        """**.f1ls** - Formula 1 list sessions"""
+        """**.f1ls** [page] - Formula 1 list sessions"""
+        page = 1
+        if len(ctx.message.content.split()[1:]):
+            with suppress(Exception):
+                page = int(ctx.message.content.split()[1])
+
+        events = "\n".join(await self.get_events(10, page, more=True))
         async with ctx.message.channel.typing():
-            return await ctx.message.channel.send("\n".join(await self.get_events(10)))
+            return await ctx.message.channel.send(events)
 
 
 def setup(bot):
