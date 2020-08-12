0a9f12952d45c756a617c53dce7fb09c6163b549
DawnKai <dawnwender@gmail.com>
2020-08-12 19:16:54 +0200

    cogs/youtube: Add youtube cog

--- super/utils/__init__.py
+++ super/utils/__init__.py
@@ -3,5 +3,6 @@ from .eightball import eightball_options
 from .funny_text import owoify
 from .fuzzy import fuz
 from .redis import SuperRedis
+from .voice import get_voice_client, get_user_voice_channel
 
 R = SuperRedis(host=settings.SUPER_REDIS_HOST, port=settings.SUPER_REDIS_PORT,)
