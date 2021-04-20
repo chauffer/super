5c64445e6f41d655c09b5f27e06b30e9a6713ac3
DawnKai <dawnwender@gmail.com>
2021-04-20 10:08:10 +0200

    super: Add Intents

--- super/run.py
+++ super/run.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 import structlog
 
 from . import settings
@@ -7,10 +8,13 @@ from .utils import R
 description = "<https://github.com/chauffer/super|super>"
 
 bot = commands.Bot(
-    command_prefix=settings.SUPER_PREFIX, description=description, pm_help=None
+    command_prefix=settings.SUPER_PREFIX,
+    description=description,
+    pm_help=None,
+    intents=discord.Intents.all(),
 )
 
-logger = structlog.getLogger('super')
+logger = structlog.getLogger("super")
 
 extensions = [
     "super.cogs.np",
@@ -43,7 +47,10 @@ def main():
             logger.debug("super/load_cogs: Loaded", extension=extension)
         except Exception as e:
             logger.error(
-                "super/load_cogs: Failed to load", extension=extension, err_type=type(e).__name__, err=e
+                "super/load_cogs: Failed to load",
+                extension=extension,
+                err_type=type(e).__name__,
+                err=e,
             )
 
     bot.run(settings.SUPER_DISCORD_TOKEN)
